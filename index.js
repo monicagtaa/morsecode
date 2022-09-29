369e2104ed078753348ed0ce96570dd17f0ba787
monicagt <monicagt@gmail.com>
2022-09-29 00:32:39 -0400

    chnages to index.js

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ import {translateToMorseCode} from "./morseTranslator.js";
 
 const inputStr = document.querySelector(".converted-text");
 console.log(inputStr);
-
+console.log (translateToMorseCode);
